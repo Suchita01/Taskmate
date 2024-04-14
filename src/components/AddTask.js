78cc7951eb180664561725a9536f68024e7a63fc
Suchita Chaudhary <66513629+Suchita01@users.noreply.github.com>
2024-04-15 02:20:12 +0530

    description required removed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,7 +3,7 @@ const AddTask = ({handleSubmit, editid, task, setTask}) => {
       <section className='addTask'>
         <form onSubmit={handleSubmit}>
           <textarea rows={1} type="text" name="title" required value={task.title} autoComplete="off" placeholder="Task" maxLength="20" onChange={(e) => setTask({...task, [e.target.name]: e.target.value})}/>
-          <textarea rows={3} name="desc" required value={task.desc} autoComplete="off" placeholder="Description" maxLength="50" onChange={(e) => setTask({...task, [e.target.name]: e.target.value})}/>
+          <textarea rows={3} name="desc" value={task.desc} autoComplete="off" placeholder="Description" maxLength="50" onChange={(e) => setTask({...task, [e.target.name]: e.target.value})}/>
           <button type="submit">{ editid ? "UPDATE" : "ADD"}</button>
         </form>
       </section>
